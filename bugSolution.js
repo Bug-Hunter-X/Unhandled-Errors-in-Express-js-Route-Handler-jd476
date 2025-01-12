9ad519cef4344cf275e822b54075b3714790eb75
BugHunter-X <askzenai@gmail.com>
2025-01-12 09:54:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  db.getUser(userId, (err, user) => {
+    if (err) {
+      console.error('Database error:', err);
+      res.status(500).send('Database error');
+    } else if (!user) {
+      res.status(404).send('User not found');
+    } else {
+      res.json(user);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
